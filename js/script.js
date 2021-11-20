dd50938cd0eb5be9faaa6e060ac311216c2e6b04
Kunal Ahirrao <erahirraokunal@gmail.com>
2021-11-20 17:23:26 +0530

    Added css and updated HTML

--- js/script.js
+++ js/script.js
@@ -1,79 +1,59 @@
-
 (function ($) {
-    "use strict";
-      $('.sakura-falling').sakura();
+  "use strict";
+  $(".sakura-falling").sakura();
 })(jQuery);
 
-$(document).on('click', function(){
-    document.getElementById("my_audio").play();
-    console.log('Shaadi me zaroor aana');
+$(document).ready(function () {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+  console.log(params);
+  if (params.name) {
+    document.getElementById("dear").innerHTML = "Dear,";
+    document.getElementById("name").innerHTML = params.name + " & Family";
+  }
+
+  document.getElementById("second_name").innerHTML =
+    "<h1>Megha</h1><p>(Daughter of Nandakumar & Sangeeta Vispute)</p>";
+  document.getElementById("first_name").innerHTML =
+    "<h1>Kunal </h1><p>(Son of Vikas & Kavita Ahirrao)</p>";
+  document.getElementById("invitation_card").innerHTML =
+    '<a href="https://raw.githubusercontent.com/kunalahirrao/weddintInvitation/main/invitation/myInvitation.pdf" download="Invitation | 28 Dec | Hope to see you there!"><div class="venue">DOWNLOAD INVITATION CARD</div>';
+  document.getElementById("mobile_number").innerHTML =
+    "<a href=”tel:9226259621>+91 9226259621</a>";
 });
 
-// Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2021 00:00:00").getTime();
+$(document).on("click", function () {
+  document.getElementById("my_audio").play();
+});
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
-    }
-}, 1000);
-
-// being a bit cool :p  
+// being a bit cool :p
 var styles = [
-    'background: linear-gradient(#D33106, #571402)'
-    , 'border: 4px solid #3E0E02'
-    , 'color: white'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
-    , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
-    , 'line-height: 40px'
-    , 'text-align: center'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
-].join(';');
+  "background: linear-gradient(#D33106, #571402)",
+  "border: 4px solid #3E0E02",
+  "color: white",
+  "display: block",
+  "text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)",
+  "box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset",
+  "line-height: 40px",
+  "text-align: center",
+  "font-weight: bold",
+  "font-size: 32px",
+].join(";");
 
 var styles1 = [
-    'color: #FF6C37'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 40px'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
-].join(';');
+  "color: #FF6C37",
+  "display: block",
+  "text-shadow: 0 2px 0 rgba(0, 0, 0, 1)",
+  "line-height: 40px",
+  "font-weight: bold",
+  "font-size: 32px",
+].join(";");
 
 var styles2 = [
-    'color: teal'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 40px'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
-].join(';');
-
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
-
-console.log('%cYour presence is requested!%c\n\nRegards: Kunal Ahirrao', styles1, styles2);
-
-console.log(
-    `%cShaadi me zaroor aana!\n\n`,
-    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
-)
+  "color: teal",
+  "display: block",
+  "text-shadow: 0 2px 0 rgba(0, 0, 0, 1)",
+  "line-height: 40px",
+  "font-weight: bold",
+  "font-size: 32px",
+].join(";");
